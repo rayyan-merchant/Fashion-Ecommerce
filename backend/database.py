d9192e278aa2fa673b7de5a96bd7802f109cfe1c
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2025-11-14 15:44:25 +0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Example: update with your own credentials
+# Update these credentials if needed
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/FashionDb"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
